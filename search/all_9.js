34333b97e429e373334a0a7b94458f02829e2c62
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-01-14 19:35:35 +0000

    Deploy ldhLogistica/logisticaTerrestre to github.com/ldhLogistica/logisticaTerrestre.git:gh-pages

--- search/all_9.js
+++ search/all_9.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['linearectatxt',['lineaRectaTxt',['../classproyecto_1_1_info_vehicle_g_u_i.html#adee8a1c0ce5158da0864dd714d976674',1,'proyecto::InfoVehicleGUI']]],
   ['listavehiculos',['listaVehiculos',['../classproyecto_1_1_logistica_g_u_i.html#a6a8174214e99ba323bd8a395936a2ac9',1,'proyecto::LogisticaGUI']]],
-  ['logger',['LOGGER',['../classproyecto_1_1vehiculos_1_1_vehiculo.html#a16e16ae683dce11fbc6c9c0ce857c2a2',1,'proyecto::vehiculos::Vehiculo']]],
+  ['logger',['LOGGER',['../classproyecto_1_1_graph.html#a80b26b33f81c49b300329cc4e7e35aa3',1,'proyecto.Graph.LOGGER()'],['../classproyecto_1_1vehiculos_1_1_vehiculo.html#a16e16ae683dce11fbc6c9c0ce857c2a2',1,'proyecto.vehiculos.Vehiculo.LOGGER()']]],
   ['logisticagui',['LogisticaGUI',['../classproyecto_1_1_logistica_g_u_i.html',1,'proyecto']]],
   ['logisticagui_2ejava',['LogisticaGUI.java',['../_logistica_g_u_i_8java.html',1,'']]],
   ['logisticaterrestretest',['LogisticaTerrestreTest',['../classproyecto_1_1_logistica_terrestre_test.html',1,'proyecto']]],
--- search/variables_9.js
+++ search/variables_9.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['linearectatxt',['lineaRectaTxt',['../classproyecto_1_1_info_vehicle_g_u_i.html#adee8a1c0ce5158da0864dd714d976674',1,'proyecto::InfoVehicleGUI']]],
   ['listavehiculos',['listaVehiculos',['../classproyecto_1_1_logistica_g_u_i.html#a6a8174214e99ba323bd8a395936a2ac9',1,'proyecto::LogisticaGUI']]],
-  ['logger',['LOGGER',['../classproyecto_1_1vehiculos_1_1_vehiculo.html#a16e16ae683dce11fbc6c9c0ce857c2a2',1,'proyecto::vehiculos::Vehiculo']]],
+  ['logger',['LOGGER',['../classproyecto_1_1_graph.html#a80b26b33f81c49b300329cc4e7e35aa3',1,'proyecto.Graph.LOGGER()'],['../classproyecto_1_1vehiculos_1_1_vehiculo.html#a16e16ae683dce11fbc6c9c0ce857c2a2',1,'proyecto.vehiculos.Vehiculo.LOGGER()']]],
   ['logisticaterrestretxt',['logisticaTerrestreTxt',['../classproyecto_1_1_logistica_g_u_i.html#a63d7dcb0bc20a72ce122b528bb32ac04',1,'proyecto::LogisticaGUI']]]
 ];
